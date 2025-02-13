e917dcdd0a2bf6e82db4dda8d3c03cc8b12b7d39
a104089 <a104089@alunos.uminho.pt>
2025-02-13 00:17:51 +0000

    Simplificação e melhoria do programa relativo ao TPC 1

--- TPC1/tpc.py
+++ TPC1/tpc.py
@@ -1,35 +1,31 @@
 import sys
 
-def somadorOnOff(texto):
-    soma = 0
-    on = 1
+soma = 0
+on = True
 
+for linha in sys.stdin:
     i = 0
-    while i < len(texto):
-        if texto[i] == '=':
+    while i < len(linha):
+        if linha[i] == '=':
             print(soma)
             i += 1
-        elif texto[i] == 'o' or texto[i] == 'O':
-            if i < len(texto)-1 and (texto[i+1] == 'n' or texto[i+1] == 'N'):
-                on = 1
-                i += 1
-            elif i < len(texto)-2 and (texto[i+1] == 'f' or texto[i+1] == 'F') and (texto[i+2] == 'f' or texto[i+2] == 'F'):
-                on = 0
-                i += 1
+        elif linha[i] == 'o' or linha[i] == 'O':
+            if i < len(linha)-1 and (linha[i+1] == 'n' or linha[i+1] == 'N'):
+                on = True
+                i += 2
+            elif i < len(linha)-2 and (linha[i+1] == 'f' or linha[i+1] == 'F') and (linha[i+2] == 'f' or linha[i+2] == 'F'):
+                on = False
+                i += 3
             else:
                 i += 1
-        elif texto[i] in "0123456789":
-            if on == 1:
+        elif linha[i] in "0123456789":
+            if on:
                 numero = 0
-                while texto[i] in "0123456789":
-                    numero = numero*10 + int(texto[i])
+                while linha[i] in "0123456789":
+                    numero = numero*10 + int(linha[i])
                     i += 1
                 soma += numero
             else:
                 i += 1
         else:
             i += 1
-
-
-entrada = sys.stdin.read()
-somadorOnOff(entrada)
