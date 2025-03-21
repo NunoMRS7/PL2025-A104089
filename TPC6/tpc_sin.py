0e0448adba9ea2d2e53356aaba5c86bde0160582
a104089 <a104089@alunos.uminho.pt>
2025-03-21 19:00:42 +0000

    feat: alteração da gramática do analisador sintático relativo ao TPC 6

--- TPC6/tpc_sin.py
+++ TPC6/tpc_sin.py
@@ -2,45 +2,52 @@ import ply.yacc as yacc
 from tpc_lex import tokens
 import sys
 
-precedence = (
-    ('left', 'ADD', 'SUB'),
-    ('left', 'MUL', 'DIV'),
-)
-
 def p_geral(p):
     """
     S : Exp
     """
     p[0] = p[1]
-    print(p[0])
-
-def p_MUL(p):
-    """
-    Exp : Exp MUL Exp
-    """
-    p[0] = p[1] * p[3]
-
-def p_DIV(p):
-    """
-    Exp : Exp DIV Exp
-    """
-    p[0] = p[1] / p[3]
+    print("Resultado =", p[0])
 
 def p_ADD(p):
     """
-    Exp : Exp ADD Exp
+    Exp : Exp ADD Termo
     """
     p[0] = p[1] + p[3]
 
 def p_SUB(p):
     """
-    Exp : Exp SUB Exp
+    Exp : Exp SUB Termo
     """
     p[0] = p[1] - p[3]
 
+def p_TERMO(p):
+    """
+    Exp : Termo
+    """
+    p[0] = p[1]
+
+def p_MUL(p):
+    """
+    Termo : Termo MUL Fator
+    """
+    p[0] = p[1] * p[3]
+
+def p_DIV(p):
+    """
+    Termo : Termo DIV Fator
+    """
+    p[0] = p[1] / p[3]
+
+def p_FATOR(p):
+    """
+    Termo : Fator
+    """
+    p[0] = p[1]
+
 def p_NUM(p):
     """
-    Exp : NUM
+    Fator : NUM
     """
     p[0] = int(p[1])
 
@@ -54,6 +61,6 @@ for linha in sys.stdin:
     parser.success = True
     parser.parse(linha)
     if parser.success:
-        print("Resultado =", linha)
+        print("Expressão válida!")
     else:
         print("Não é possivel gerar um resultado pois a expressão é inválida")
